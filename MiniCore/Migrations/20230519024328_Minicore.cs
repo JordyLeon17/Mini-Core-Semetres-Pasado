c04da56d2de0476ffbc0774316c460a7cc05df71
JordyLeon17 <jordyn2010@hotmail.es>
2023-05-18 22:55:12 -0500

    Mini Core Semestre Pasado

---
+++ MiniCore/Migrations/20230519024328_Minicore.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiniCore.Migrations
+{
+    public partial class Minicore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
